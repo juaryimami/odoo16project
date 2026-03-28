547ff8fa4c5467bc4bacb567105231e65e165a4b
juhar yimer <juharyimer7@gmail.com>
2026-03-28 11:41:11 +0300

    initial commit

---
+++ custom_addons/muk_web_theme/footer.js
@@ -0,0 +1,7 @@
+var ti=document.createElement('div');
+ti.createTextNode("Ashewa Technologies 2022 Design Protected");
+var cor=document.getElementsByTagName('body');
+if(cor.appendChild(ti))
+{
+	console.log("try again!");
+}
---
+++ custom_addons/muk_web_theme/static/src/footer.js
@@ -0,0 +1,5 @@
+const qq=document.createElement("div");
+qq.id="footer";
+const p=document.getElementsByClassName("o_web_client");
+p[0].appendChild(qq);
+qq.innerHTML+="<p> &copy; COPY RIGHT 2026 NEMAL ENGINEERING P.L.C ALL RIGHT RESERVED </p>";

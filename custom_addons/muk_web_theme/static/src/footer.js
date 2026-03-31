22b0a501c1baa05530042ace18f540b0822dccee
juhar yimer <juharyimer7@gmail.com>
2026-03-31 10:44:22 +0300

    them added

--- custom_addons/muk_web_theme/static/src/footer.js
+++ custom_addons/muk_web_theme/static/src/footer.js
@@ -2,4 +2,4 @@ const qq=document.createElement("div");
 qq.id="footer";
 const p=document.getElementsByClassName("o_web_client");
 p[0].appendChild(qq);
-qq.innerHTML+="<p> &copy; COPY RIGHT 2026 NEMAL ENGINEERING P.L.C ALL RIGHT RESERVED </p>";
+qq.innerHTML+="<p> &copy; COPY RIGHT 2025 E-HEALTH IT SERVICE PLC. ALL RIGHT RESERVED </p>";
